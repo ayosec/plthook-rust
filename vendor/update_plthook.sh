e669a41fc88a693337b751b07f0298e78673be7b
Ayose <ayosec@gmail.com>
2021-07-28 20:12:40 +0100

    Script to download code from plthook repository.
    Extract only required files, and put them in vendor/plthook directory.

---
+++ vendor/update_plthook.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Download file source files for the latest version of plthook.
+#
+# It requires the bsdtar and curl/wget commands.
+#
+# Usage:
+#
+#   $ cd vendor
+#   $ ./update_plthook.sh [branch]
+
+set -euo pipefail
+
+if [ -z "$(command -v bsdtar)" ]
+then
+  echo Missing bsdtar command.
+  exit 1
+fi
+
+BRANCH=${1:-master}
+
+download() {
+  if [ -n "$(command -v wget)" ]
+  then
+    wget -qO - "$1"
+  elif [ -n "$(command -v curl)" ]
+  then
+    curl -sL "$1"
+  else
+    echo "Both wget and curl are missing." 1>&2
+    exit 1
+  fi
+}
+
+cd "$(dirname "$0")"
+mkdir -p plthook
+cd plthook
+
+download "https://github.com/kubo/plthook/archive/refs/heads/$BRANCH.zip" | \
+  bsdtar --strip-components 1 -xvf - \
+    "plthook-$BRANCH"/plthook{.h,_elf.c,_osx.c,_win32.c}
