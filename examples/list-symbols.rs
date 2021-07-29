1880da173b93bf1288eed2fc05c3aee9eac92ea5
Ayose <ayosec@gmail.com>
2021-07-29 01:27:41 +0100

    List symbols from shared objects in list-symbols example.

--- examples/list-symbols.rs
+++ examples/list-symbols.rs
@@ -1,8 +1,28 @@
-//! Print all symbols in the object file.
+//! Print all symbols in object files.
+//!
+//! It accepts shared objects as command line arguments.
+
+use plthook::ObjectFile;
+use std::env;
 
 fn main() {
-    let object = plthook::ObjectFile::open_main_program().unwrap();
+    let object = ObjectFile::open_main_program().expect("Failed to open main program");
+    print_symbols(object);
 
+    for path in env::args_os().skip(1) {
+        println!("\n{:?}", path);
+        match ObjectFile::open_file(path) {
+            Ok(o) => print_symbols(o),
+            Err(e) => eprintln!(
+                "{}\nlast_error_message = {}",
+                e,
+                plthook::last_error_message()
+            ),
+        };
+    }
+}
+
+fn print_symbols(object: ObjectFile) {
     for symbol in object.symbols() {
         println!("{:?} {:?}", symbol.func_address, symbol.name);
     }
